634dd417bb73dfc6622ef9f4ea4b068504e81103
Guus der Kinderen <guus.der.kinderen@gmail.com>
2025-07-17 12:15:41 +0200

    Review feedback: fix copy/paste error.
    Co-authored-by: Dan Caseley <dan@caseley.me.uk>

--- src/main/java/org/igniterealtime/smack/inttest/rfc6121/RFC6121Section2dot4RosterIntegrationTest.java
+++ src/main/java/org/igniterealtime/smack/inttest/rfc6121/RFC6121Section2dot4RosterIntegrationTest.java
@@ -154,7 +154,7 @@ public class RFC6121Section2dot4RosterIntegrationTest extends AbstractSmackInteg
             roster.reloadAndWait();
 
             final RosterEntry rosterItem = roster.getEntry(target);
-            assertEquals("Romeo", rosterItem.getName(), "Unexpected name for the roster item of '" + target + "' on the roster of '" + conOne.getUser() + "': Expected to name to be equal to that what was used in a Roster Update that intended to add a group to a preexisting roster item (but it was not).");
+            assertEquals("Romeo", rosterItem.getName(), "Unexpected name for the roster item of '" + target + "' on the roster of '" + conOne.getUser() + "': Expected to name to be equal to that what was used in a Roster Update that intended to remove all groups from a preexisting roster item (but it was not).");
             assertEquals(0, rosterItem.getGroups().size(), "Unexpected amount of groups for the roster item of '" + target + "' on the roster of '" + conOne.getUser() + "': Expected to find no groups after removing all groups from a preexisting roster item (but the group list is not empty).");
         } catch (XMPPException.XMPPErrorException e) {
             fail("Unexpected error response received by '" + conOne.getUser() + "' after it sent a a Roster Update that intended to remove the last group from a preexisting roster item.");
