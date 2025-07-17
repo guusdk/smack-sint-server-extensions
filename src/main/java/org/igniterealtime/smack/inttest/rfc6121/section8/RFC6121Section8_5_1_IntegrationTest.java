6957baec7ce44359d937fcae4af2849b9344bec7
Guus der Kinderen <guus.der.kinderen@gmail.com>
2025-07-17 12:11:21 +0200

    Review feedback: fix copy/paste error.
    Co-authored-by: Dan Caseley <dan@caseley.me.uk>

--- src/main/java/org/igniterealtime/smack/inttest/rfc6121/section8/RFC6121Section8_5_1_IntegrationTest.java
+++ src/main/java/org/igniterealtime/smack/inttest/rfc6121/section8/RFC6121Section8_5_1_IntegrationTest.java
@@ -212,7 +212,7 @@ public class RFC6121Section8_5_1_IntegrationTest extends AbstractSmackIntegratio
         doTestLocalDomainNoSuchUserPresenceOfType(localDomainNoSuchUser, Presence.Type.subscribed);
     }
 
-    @SmackIntegrationTest(section = "8.5.1", quote = "If the 'to' address specifies a bare JID <localpart@domainpart> [...] where the domainpart of the JID matches a configured domain that is serviced by the server itself, the server MUST proceed as follows. [...] If the user account identified by the 'to' attribute does not exist, how the stanza is processed depends on the stanza type. [...] For a presence stanza with [...] a 'type' attribute of \"unsubscribe\" the server MUST silently ignore the stanza.")
+    @SmackIntegrationTest(section = "8.5.1", quote = "If the 'to' address specifies a bare JID <localpart@domainpart> [...] where the domainpart of the JID matches a configured domain that is serviced by the server itself, the server MUST proceed as follows. [...] If the user account identified by the 'to' attribute does not exist, how the stanza is processed depends on the stanza type. [...] For a presence stanza of type [...] \"unsubscribe\" [...] the server MUST silently ignore the stanza.")
     public void testLocalDomainNoSuchUserPresenceUnsubscribeTypeBareJid() throws XmppStringprepException, SmackException.NotConnectedException, SmackException.NoResponseException, InterruptedException, XMPPException.XMPPErrorException
     {
         // Setup test fixture.
